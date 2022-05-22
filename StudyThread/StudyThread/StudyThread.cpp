d9932e27eeac7f61bec147f9e698a2c0ccc6df49
好好水 <1441641216@qq.com>
2022-05-23 07:31:00 +0800

    add thread pool

--- StudyThread/StudyThread/StudyThread.cpp
+++ StudyThread/StudyThread/StudyThread.cpp
@@ -8,32 +8,55 @@
 #include "CDeleteActor.h"
 #include "CShowActor.h"
 #include "CStudyList.h"
+#include"ThreadPool.h"
 
+
+
+void func()
+{
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	std::cout << "worker thread ID:" << std::this_thread::get_id() << std::endl;
+
+}
 int main()
 {
-	std::shared_ptr<IStudyStl> pStudylist = CStudyList::GetInstance();
-	IStudyStl* pList = pStudylist.get();
+	//std::shared_ptr<IStudyStl> pStudylist = CStudyList::GetInstance();
+	//IStudyStl* pList = pStudylist.get();
 
-	std::shared_ptr<CAddActor> pAddThread(new(std::nothrow) CAddActor(pList));
-	std::shared_ptr<CDeleteActor> pDeletThread(new(std::nothrow) CDeleteActor(pList));
-	std::shared_ptr<CShowActor> pShowThread(new(std::nothrow) CShowActor(pList));
+	//std::shared_ptr<CAddActor> pAddThread(new(std::nothrow) CAddActor(pList));
+	//std::shared_ptr<CDeleteActor> pDeletThread(new(std::nothrow) CDeleteActor(pList));
+	//std::shared_ptr<CShowActor> pShowThread(new(std::nothrow) CShowActor(pList));
 
-	pAddThread->Start();
-	std::this_thread::sleep_for(std::chrono::milliseconds(50));
-	pAddThread->Stop();
-
-	pShowThread->Start();
-	std::this_thread::sleep_for(std::chrono::milliseconds(1));
-
-	pDeletThread->Start();
-	std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	//pAddThread->Start();
+	//std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	//pAddThread->Stop();
 
 	//pShowThread->Start();
-	std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	//std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
-	pDeletThread->Stop();
-	pShowThread->Stop();
+	//pDeletThread->Start();
+	//std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
+	////pShowThread->Start();
+	//std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+	//pDeletThread->Stop();
+	//pShowThread->Stop();
+
+
+	//ThreadPool pool(4);
+
+	//// enqueue and store future
+	//auto result = pool.enqueue([](int answer) { return answer; }, 42);
+
+	//// get result from future, print 42
+	//std::cout << result.get() << std::endl;
+
+	ThreadPool pool(4);
+	while (1)
+	{
+		pool.enqueue(func);
+	}
 
 }
 
